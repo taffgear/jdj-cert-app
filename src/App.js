4cc746da4faa0ae0fa8459d07dfc89987fb8402b
markhorsman <mark.horsman@tam-one.nl>
2018-02-11 19:49:35 +0100

    Implemented dropzone and axios for uploading files

--- src/App.js
+++ src/App.js
@@ -24,8 +24,141 @@ import List, {
   ListItemText,
 } from 'material-ui/List';
 // import Icon from 'material-ui/Icon';
+import Dropzone from 'react-dropzone';
+import shortid from 'shortid';
+import axios from 'axios';
 
 import './App.css';
+
+import cnf from '../config.json';
+
+const BasicDropzone = React.createClass({
+    getInitialState() {
+        return {
+            items: [],
+            files: [],
+            queue: [],
+        };
+    },
+
+    onDrop(files) {
+        const filesWithIds = files.map(f => ({ id: shortid.generate(), file: f }));
+        this.state.items = this.state.items.concat(filesWithIds);
+        this.setState(this.state);
+    },
+
+    onSelectChange(checked, index) {
+        this.state.items[index].selected = checked;
+        this.setState(this.state);
+    },
+
+    onDelete(index) {
+        this.state.items.splice(index, 1);
+        this.setState(this.state);
+    },
+
+    readFile(file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            this.state.files.push(file);
+
+            if (this.state.files.length === this.state.queue.length) { this.upload(); }
+        };
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+
+        reader.readAsDataURL(file);
+    },
+
+    upload() {
+        const formData = new FormData();
+
+        this.state.files.forEach((file) => {
+            formData.append('documents', file);
+        });
+
+        axios.post('http://localhost:5000/files', formData, {
+            auth: {
+                username: cnf.api.auth.username,
+                password: cnf.api.auth.password,
+            } })
+            .then((response) => {
+                console.log(response);
+                this.state.items = [];
+                this.state.files = [];
+                this.state.queue = [];
+
+                this.setState(this.state);
+            })
+            .catch(console.log)
+      ;
+    },
+
+    onProcessFilesClick() {
+        this.state.items.forEach((item) => {
+            if (!item.selected) return;
+
+            this.state.queue.push(item);
+            this.readFile(item.file);
+        });
+    },
+
+    render() {
+        return (
+            <Card className="filemanager">
+                <CardContent>
+                    <Typography type="headline" component="h2" align="left">Bestanden</Typography>
+                    <form className="upload-file-form">
+                        {
+                Size(Find(this.state.items, { selected: true })) ?
+                    <Button className="process-files" raised color="primary" onClick={this.onProcessFilesClick}>Geselecteerde bestanden verwerken</Button>
+              : ''
+              }
+                        <div className="dropzone">
+                            <Dropzone
+                                onDrop={this.onDrop}
+                                accept="application/pdf">
+                                <p>Sleep bestanden in dit vak of klik op de plus knop om bestanden te selecteren.</p>
+                                <Button fab mini color="primary" aria-label="add" className="upload-file">
+                                    <AddIcon />
+                                </Button>
+                            </Dropzone>
+                        </div>
+                    </form>
+                    <List>
+                        <ScrollArea
+                            speed={0.8}
+                            className="files-list"
+                            horizontal={false}
+                            minScrollSize={40}>
+                            {this.state.items.map((obj, index) => (
+                                <ListItem
+                                    key={obj.id}
+                                    dense
+                                    button>
+                                    <Checkbox
+                                        checked={obj.selected}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        onClick={e => this.onSelectChange(e.target.checked, index)} />
+                                    <ListItemText primary={obj.file.name} />
+                                    <ListItemSecondaryAction>
+                                        <IconButton aria-label="Verwijderen" onClick={() => this.onDelete(index)}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+            ))}
+                        </ScrollArea>
+
+                    </List>
+                </CardContent>
+            </Card>
+
+        );
+    },
+});
+
 
 const LogComponent = props => (
     <List className="logs-list">
@@ -121,55 +254,6 @@ ViewLogs.propTypes = {
             date: PropTypes.string.isRequired,
         })).isRequired,
     }).isRequired,
-};
-
-const FileManager = props => (
-    <Card className="filemanager">
-        <CardContent>
-            <Typography type="headline" component="h2" align="left">Bestanden</Typography>
-            {
-              Size(Find(props.files, { selected: true })) ?
-                  <Button raised color="primary" onClick={props.onProcessFilesClick}>Geselecteerde bestanden verwerken</Button>
-            : ''
-            }
-            <List>
-                {props.files.map((file, index) => (
-                    <ListItem
-                        key={file.id}
-                        dense
-                        button>
-                        <Checkbox
-                            checked={file.selected}
-                            tabIndex={-1}
-                            disableRipple
-                            onClick={e => props.onSelectChange(e.target.checked, index)} />
-                        <ListItemText primary={file.name} />
-                        <ListItemSecondaryAction>
-                            <IconButton aria-label="Verwijderen" onClick={() => props.onDelete(index)}>
-                                <DeleteIcon />
-                            </IconButton>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-          ))}
-            </List>
-
-            <form className="upload-file-form">
-                <Button fab mini color="primary" aria-label="add" className="upload-file">
-                    <AddIcon />
-                </Button>
-
-            </form>
-        </CardContent>
-    </Card>
-    );
-
-FileManager.propTypes = {
-    // onSelectChange: PropTypes.func.isRequired,
-    files: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        selected: PropTypes.bool.isRequired,
-    })).isRequired,
 };
 
 const Status = props => (
@@ -294,12 +378,7 @@ class App extends React.Component {
             { id: 4, title: 'Artikel KRLOO603', date: Moment().format('DD-MM-YYYY') },
             { id: 5, title: 'Artikel KRLOO604', date: Moment().format('DD-MM-YYYY') },
             ],
-        },
-        files: [
-          { id: 1, name: `${Moment().format('YYYY-MM-DD')}-certificate-0067RF.pdf`, selected: false },
-          { id: 2, name: `${Moment().format('YYYY-MM-DD')}-certificate-0067RF.pdf`, selected: false },
-          { id: 3, name: `${Moment().format('YYYY-MM-DD')}-certificate-0067RF.pdf`, selected: false },
-        ],
+        }
     };
 
     onScheduledStartChanged = (e) => {
@@ -319,21 +398,6 @@ class App extends React.Component {
 
     onLogTabsChange = (event, value) => {
         this.state.activeLogsTab = value;
-        this.setState(this.state);
-    };
-
-    onFileSelectChange = (checked, index) => {
-        this.state.files[index].selected = checked;
-        this.setState(this.state);
-    };
-
-    onFileDelete = (index) => {
-        this.state.files.splice(index, 1);
-        this.setState(this.state);
-    };
-
-    onProcessFilesClick = () => {
-        this.state.files = [];
         this.setState(this.state);
     };
 
@@ -358,11 +422,7 @@ class App extends React.Component {
                         <Status running={this.state.running} />
                     </Grid>
                     <Grid item xs={8}>
-                        <FileManager
-                            files={this.state.files}
-                            onSelectChange={this.onFileSelectChange}
-                            onDelete={this.onFileDelete}
-                            onProcessFilesClick={this.onProcessFilesClick} />
+                        <BasicDropzone />
                     </Grid>
                 </Grid>
 
